e6f8325e3a4670f24e5b6f272aba9938c1e73dbd
e-bierschenk <bushanks@gmail.com>
2022-02-17 10:23:46 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+/* <section id="computer--1">
+	<h1>Apple Macbook</h1>
+	<div>Model: Pro</div>
+	<div>Memory: 32 GB</div>
+	<div>Hard drive space: 500 GB</div>
+	<div>Processor speed: 2.4 Ghz</div>
+</section> 
+*/
+
+const computer = {
+	id: 1,
+	manufacturer: "Apple",
+	make: "Macbook",
+	model: "Pro",
+	specs: {
+		memory: 32,
+		hardDrive: "500 GB",
+		processor: 2.4
+	}
+}
+
+const htmlString = (obj) => {
+    return `<section id="computer--${obj.id}">\n
+                <h1>${obj.manufacturer} ${obj.make}</h1>\n
+                <div>Model: ${obj.model}</div>\n
+                <div>Memory: ${obj.specs.memory}</div>\n
+                <div>Hard drive space: ${obj.specs.hardDrive}</div>\n
+                <div>Processor speed: ${obj.specs.processor}</div>\n
+            </section>`
+}
+
+console.log(htmlString(computer))
